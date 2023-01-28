e3360e63792d5c445e91170e3c8d684cdea82649
Romain Bacque <bacqueromain@orange.fr>
2023-01-28 20:13:18 +0100

    feat: add HTML sanitizing

--- app/validation/schemas.js
+++ app/validation/schemas.js
@@ -1,8 +1,33 @@
-const joi = require("joi");
+const baseJoi = require("joi");
+const sanitizeHtml = require("sanitize-html");
+
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {
+    "string.escapeHTML": "{{#label}} must not include HTML!",
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean == value) {
+          return clean;
+        }
+        return helpers.error("string.escapeHTML");
+      },
+    },
+  },
+});
+
+const joi = baseJoi.extend(extension);
 
 module.exports.cocktailSchema = joi
   .object({
-    name: joi.string().required(),
+    name: joi.string().escapeHTML().required(),
     details: joi
       .array()
       .items({
@@ -15,14 +40,13 @@ module.exports.cocktailSchema = joi
 
 module.exports.ingredientSchema = joi
   .object({
-    name: joi.string().required(),
+    name: joi.string().escapeHTML().required(),
     unitId: joi.number().required(),
   })
   .required();
 
 module.exports.unitSchema = joi
   .object({
-    id: joi.number(),
-    title: joi.string().required(),
+    title: joi.string().escapeHTML().required(),
   })
   .required();
