a012ec613163e7edc82b8f7f94ab8c8f6a8a7415
Romain Bacque <bacqueromain@orange.fr>
2023-01-31 15:57:15 +0100

    feat: add credentials option

--- app/index.js
+++ app/index.js
@@ -3,9 +3,7 @@ const app = express();
 const router = require("./routers/");
 const cors = require("cors");
 
-app.use(cors({ origin: "*" }));
-
-app.use(express.static("public"));
+app.use(cors({ origin: "*", credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
