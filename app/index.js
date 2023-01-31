218037dea3a98cef035bc24b0a3580b467928588
Romain Bacque <bacqueromain@orange.fr>
2023-01-31 15:21:22 +0100

    feat: remove and uninstall swagger modules

--- app/index.js
+++ app/index.js
@@ -2,28 +2,6 @@ const express = require("express");
 const app = express();
 const router = require("./routers/");
 const cors = require("cors");
-const expressJSDocSwagger = require("express-jsdoc-swagger");
-
-const options = {
-  info: {
-    version: "1.0.0",
-    title: "Cocktail API",
-    license: {
-      name: "MIT",
-    },
-  },
-  security: {
-    BasicAuth: {
-      type: "http",
-      scheme: "basic",
-    },
-  },
-  swaggerUIPath: "/cocktailapidoc",
-  baseDir: __dirname,
-  filesPattern: "./**/*.js",
-};
-
-expressJSDocSwagger(app)(options);
 
 app.use(cors({ origin: "*" }));
 
